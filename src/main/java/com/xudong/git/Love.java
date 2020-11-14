1faa5ba9198e25296fe6bfd78232ff29b786b212
xjb_global <xjb12345_global@163.com>
2020-11-14 17:59:27 +0800

    commit ssss

--- src/main/java/com/xudong/git/Love.java
+++ src/main/java/com/xudong/git/Love.java
@@ -4,6 +4,10 @@ public class Love {
 	
 	public static void main(String[] args) {
 		System.out.println("ÄãºÃ");
+		System.out.println("gitDemo2");
+		
 		System.out.println("gitDemo1");
+		System.out.println("¹þ¹þ¹þ");
+		int a = 0;
 	}
 }
